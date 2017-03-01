bebc45d00db25813b0b0853de8ef4b9f41392f84
Matteo Spreafico <matteo.spreafico@workday.com>
2017-03-01 19:06:02 +0000

    test

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { endpoint } from './endpoint';
+import { cache } from './cache';
+import { alg1 } from './alg1';
+import { input } from './input';
+
+let inputData = new input('./data/kittens.in');
+
+let caches: cache[] = inputData.caches;
+// let cacheSize = inputData.cacheSize;
+let endpoints: endpoint[] = inputData.endpoints;
+
+let alg = new alg1(endpoints, caches);
+alg.compute();
+alg.output();
